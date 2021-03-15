55f24aa709b6f1305d9b72c473e5dc5f05a5f3cd
alimran halmamat <mr.uygur1996@gmail.com>
2021-03-14 23:53:14 -0700

    linkedlist insertion

--- algo_bootcamp/Data structures/Linked Lists/LinkedList.js
+++ algo_bootcamp/Data structures/Linked Lists/LinkedList.js
@@ -18,22 +18,58 @@ class LinkedList {
         return this
     }
 
-    prepend(value){
+    prepend(value) {
         // add value to the beginning of the linked list
         const newNode = {
-            value:value,
-            next:null
+            value: value,
+            next: null
         }
         newNode.next = this.head
         this.head = newNode
         return this
 
     }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array;
+      }
+    insert(index, value) {
+        // if added index is greater than the linkedlist length then add value to the tail 
+        if(index >= this.length){
+            return this.append(value)
+        }
+        // create newNode 
+        const newNode = {
+            value:value,
+            next:null
+        }
+        // get to the node index
+        const leader = this.findIndex(index-1)
+        const holdingPointer = leader.next 
+        leader.next = newNode
+        newNode.next = holdingPointer
+        this.length++
+        return this.printList()
+    }
+    findIndex(index){
+        let currentNode = this.head;
+        let count = 0
 
-
+        while( count !== index){
+                currentNode = currentNode.next
+                count++
+    }
+    return currentNode
+}
 }
 const myList = new LinkedList(100)
 myList.append(200)
 myList.append(300)
-myList. prepend(700)
-console.log(myList)
+myList.prepend(700)
+myList.insert(2,99)
+myList.printList()
