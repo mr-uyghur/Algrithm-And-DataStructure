fade5cb130609f23ef76e08f14f3bcc46dfe4b51
alimran halmamat <mr.uygur1996@gmail.com>
2021-03-18 19:21:07 -0700

    doubly linked list practice

--- Codecademy/Data Structures/Doublt LinkedLists/DoublyLinkedList.js
+++ Codecademy/Data Structures/Doublt LinkedLists/DoublyLinkedList.js
@@ -19,7 +19,56 @@ class DoublyLinkedList {
         }
     }
 
-    // Create your .addToTail() method below:
+    addToTail(data) {
+        const newTail = new Node(data);
+        const currentTail = this.tail;
+        if (currentTail) {
+            currentTail.setNextNode(newTail);
+            newTail.setPreviousNode(currentTail);
+        }
+        this.tail = newTail;
+        if (!this.head) {
+            this.head = newTail;
+        }
+    }
+
+    removeHead() {
+        const removedHead = this.head;
+        if (!removedHead) {
+            return;
+        }
+        this.head = removedHead.getNextNode();
+        if (this.head) {
+            this.head.setPreviousNode(null);
+        }
+        if (removedHead === this.tail) {
+            this.removeTail();
+        }
+        return removedHead.data;
+    }
+
+    // Create your .removeTail() method below:
+    removeTail() {
+        const removedTail = this.tail
+        // if The list is empty, so there’s nothing to return, and the method ends
+        if (!removedTail) {
+            return
+        }
+        // update the list’s tail to be the current tail’s previous node
+        this.tail = removedTail.getPreviousNode()
+        // If there is still a tail to the list (meaning the list had more than one element when we started)
+        // Set the tail’s next node to null
+        if (this.tail) {
+            this.tail.setNextNode(null)
+        }
+        // If the removed tail was also the head of the list (meaning there was only one element in the list), call .removeHead() to make the necessary changes to the head of the list
+        if (removedTail === this.head) {
+            this.removeHead()
+        }
+        // Finally, return the old tail
+        return removedTail.data
+
+    }
 
 
     printList() {
