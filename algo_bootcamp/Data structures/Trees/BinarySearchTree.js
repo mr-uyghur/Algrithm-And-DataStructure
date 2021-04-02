83758ed2e2cdcc7f7fe93e08267a3d06ffecb984
alimran halmamat <mr.uygur1996@gmail.com>
2021-04-02 16:25:52 -0700

    Binary seach remove method

--- algo_bootcamp/Data structures/Trees/BinarySearchTree.js
+++ algo_bootcamp/Data structures/Trees/BinarySearchTree.js
@@ -1,50 +1,144 @@
 // create a Binary tree structure
 
 class Node {
-    constructor(value){
+    constructor(value) {
         this.left = null
         this.right = null
         this.value = value
     }
 }
 
-class BinarySearchTree{
-    constructor(){
-        this.root= null
+class BinarySearchTree {
+    constructor() {
+        this.root = null
     }
 
-    insert(value){
+    insert(value) {
         const newNode = new Node(value)
-        if(this.root === null){
+        if (this.root === null) {
             this.root = newNode
             return this
         }
         else {
-            let currentNode = this.root 
-            while(true){ //treversing
-                if(value < currentNode.value){ //is the value less than the current root?
+            let currentNode = this.root
+            while (true) { //treversing
+                if (value < currentNode.value) { //is the value less than the current root?
                     //left
                     //logic below will keep looping till we find a node that doesn't have a left side
-                    if(!currentNode.left){ //if the left node is empty
+                    if (!currentNode.left) { //if the left node is empty
                         currentNode.left = newNode
                         return this
                     }
                     currentNode = currentNode.left //shift currentNode to its left if node left isn't null
-                } else{
+                } else {
                     //Right if value is greater than current node
                     //similar logic as above
-                    if(!currentNode.right){
+                    if (!currentNode.right) {
                         currentNode.right = newNode
                         return this
                     }
                     currentNode = currentNode.right //keep looping to the right till node right is null
                 }
-            } 
+            }
         }
 
     }
 
-    lookup(value){
+    lookup(value) {
+        if (!this.root) {
+            return false // if root don't exist
+        }
+        let currentNode = this.root
+        while (currentNode) {
+            //loop through our node to find the current node
+            if (value < currentNode.value) {
+                currentNode = currentNode.left
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right
+            } else if (currentNode.value === value) { // this condition means we have found the value
+                return currentNode // break from while loop
+            }
+        }
+        return false
+    }
 
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                //We have a match, get to work!
+
+                //Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+
+                            //if parent < current value, make left child a right child of parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+
+                    //Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+
+                            //if parent < current, make right child a right child of the parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                    //Option 3: Right child that has a left child
+                } else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
