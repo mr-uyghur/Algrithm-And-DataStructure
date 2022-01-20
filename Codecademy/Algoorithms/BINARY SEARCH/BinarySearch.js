b08c9d59551873872899cbb7e978bd1ba29e47fa
alimran halmamat <mr.uygur1996@gmail.com>
2022-01-19 23:15:20 -0800

    initiated binary tree data sctructure

--- Codecademy/Algoorithms/BINARY SEARCH/BinarySearch.js
+++ Codecademy/Algoorithms/BINARY SEARCH/BinarySearch.js
@@ -3,31 +3,31 @@ const binarySearch = (arr, target) => {
     //set 2 pointers, left and right pointers
     //pointer left points to the first index in the window 
     //pointer right points to the last index
-  let left = 0;
-  let right = arr.length;
+    let left = 0;
+    let right = arr.length
 
     //condition is as long the value of right pointer is larger than the left the loop continues
   while (right > left) {
       //grab the index index of the array, 
       //use that index to compare that value with target
-    const indexToCheck = Math.floor((left + right) / 2);
-    const midPoint = arr[indexToCheck];
-    console.log(indexToCheck);
+      let indexToCheck = Math.floor((right + left) / 2)
+      let midPoint = arr[indexToCheck]
+      console.log(midPoint)
 
-    if (midPoint === target) {
-      return indexToCheck;
-    } else if (midPoint < target) {
-      left = indexToCheck + 1;
-    } else {
-      right = indexToCheck;
-    }
+      if(midPoint === target) {
+        return indexToCheck
+      } else if(midPoint < target) {
+        left = indexToCheck + 1
+      } else {
+        right = indexToCheck
+      }
   }
 
   return null;
 };
 
 const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
-const target = 41;
+const target = 94;
 
 targetIndex = binarySearch(searchable, target);
 
---
+++ Codecademy/Data Structures/BinaryTree/scriptTest.js
@@ -0,0 +1,6 @@
+const BinaryTree = require('./BinaryTree');
+
+// create a BinaryTree here
+const bt = new BinaryTree(15);
+
+console.log(bt);
