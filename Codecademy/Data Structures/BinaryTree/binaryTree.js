adea93837559a9a03e23854d6cee54aadc95b436
alimran halmamat <mr.uygur1996@gmail.com>
2022-01-23 07:27:52 -0800

    added insert method to binary tree Data Structure

--- Codecademy/Data Structures/BinaryTree/binaryTree.js
+++ Codecademy/Data Structures/BinaryTree/binaryTree.js
@@ -1,11 +1,42 @@
 class BinaryTree {
-    constructor(value, depth = 1) {
-      this.value = value;
-      this.depth = depth;
-      this.left = null;
-      this.right = null;
+  constructor(value, depth = 1) {
+    this.value = value;
+    this.depth = depth;
+    this.left = null;
+    this.right = null;
+  }
+  //insert new value to the tree
+  /*------------------------------------------------------------
+  insersion Logic:
+  If the new value is less than the root node's value
+  If a left child node doesn't exist 
+    Create a new BinaryTree with the new value at a greater depth and assign it to the left pointer
+  Else
+    Recursively call .insert() on the left child node  
+Else
+  If a right child node doesn't exist
+    Create a new BinaryTree with the new value at a greater depth and assign it to a right pointer
+  Else
+    Recursively call .insert() on the right child node 
+  ----------------------------------------------------------------*/
+  insert(value){
+    if(value < this.value){
+      if(!this.left){
+        this.left = new BinaryTree(value, this.depth + 1);
+      }
+      else{
+        this.left.insert(value)
+      }
     }
-  };
-  
-  module.exports = BinaryTree;
-  
+    else{
+      if(!this.right){
+        this.right = new BinaryTree(value, this.depth + 1);
+      }
+      else{
+        this.right.insert(value)
+      }
+    }
+  }
+};
+
+module.exports = BinaryTree;
--- Codecademy/Data Structures/BinaryTree/scriptTest.js
+++ Codecademy/Data Structures/BinaryTree/scriptTest.js
@@ -1,6 +1,11 @@
 const BinaryTree = require('./BinaryTree');
 
 // create a BinaryTree here
-const bt = new BinaryTree(15);
+const bt = new BinaryTree(100);
 
-console.log(bt);
+// insert values to the BinaryTree here
+bt.insert(50)
+bt.insert(125)
+bt.insert(75)
+bt.insert(25)
+console.log(bt)
