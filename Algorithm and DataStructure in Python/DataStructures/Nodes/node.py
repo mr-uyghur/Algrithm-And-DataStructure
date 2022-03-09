c4fb7be322d9eacf0f441cb4d3832cc341929263
alimran halmamat <mr.uygur1996@gmail.com>
2022-03-09 00:09:10 -0800

    added Node data structure in Python

---
+++ Algorithm and DataStructure in Python/DataStructures/Nodes/node.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value, link_node=None):
+        self.value = value
+        self.link_node = link_node
+    
+    def set_link_node(self, link_node):
+        self.link_node = link_node
+    
+    def get_link_node(self):
+        return self.link_node
+  
+    def get_value(self):
+        return self.value
+
+# simple tests for node
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+yacko.set_link_node(dot)
+dot.set_link_node(wacko)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+print(wackos_data)
+print(dots_data)
