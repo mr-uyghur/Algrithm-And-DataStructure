8c22ef97c6243d51d62a597758c209682b9a7ca7
alimran halmamat <mr.uygur1996@gmail.com>
2022-08-28 15:44:03 -0700

    added tree data structure in python with remove_child funtion

---
+++ Algorithm and DataStructure in Python/DataStructures/Tree/script.py
@@ -0,0 +1,34 @@
+# Define your "TreeNode" Python class below
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+        
+
+    def add_child(self, child_node):
+        print("Adding " + child_node.value)
+        self.children.append(child_node)
+
+    def remove_child(self, child_node):
+        print("Removing" + child_node.value + "from" + self.value)
+        new_children = []
+
+        for item in self.children:
+
+            # add items from the original children of node
+            # to the new children list,
+            # only if the index item is not equal to the child_node we removing
+            if item != child_node:
+                new_children.append(item)
+
+            self.children = new_children
+            
+
+
+root = TreeNode("I am Root")
+child = TreeNode("A wee sappling")
+bad_seed = TreeNode("Root Rot!")
+
+root.add_child(child)
+root.add_child(bad_seed)
+root.remove_child(bad_seed)
