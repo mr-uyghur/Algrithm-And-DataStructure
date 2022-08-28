af6e2d57440949f7b7819b0a47b50b74232a1fe7
alimran halmamat <mr.uygur1996@gmail.com>
2022-08-28 15:47:57 -0700

    updated remove_child function of Tree DS with list comprehension

--- Algorithm and DataStructure in Python/DataStructures/Tree/script.py
+++ Algorithm and DataStructure in Python/DataStructures/Tree/script.py
@@ -3,7 +3,6 @@ class TreeNode:
     def __init__(self, value):
         self.value = value
         self.children = []
-        
 
     def add_child(self, child_node):
         print("Adding " + child_node.value)
@@ -11,18 +10,22 @@ class TreeNode:
 
     def remove_child(self, child_node):
         print("Removing" + child_node.value + "from" + self.value)
-        new_children = []
+        # new_children = []
 
         for item in self.children:
 
             # add items from the original children of node
             # to the new children list,
             # only if the index item is not equal to the child_node we removing
-            if item != child_node:
-                new_children.append(item)
+            # if item != child_node:
+            #     new_children.append(item)
 
-            self.children = new_children
+            # self.children = new_children
             
+            #updated remove function with list comprehension
+            self.children = [
+                item for item in self.children if item != child_node
+            ]
 
 
 root = TreeNode("I am Root")
