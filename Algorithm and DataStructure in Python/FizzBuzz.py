0d5dc04dbe85436f8e9f271b22e133091af8ee25
Mr-uyghur <mr.uygur1996@gmail.com>
2024-02-24 20:43:52 -0800

    added classic Fizz Buzz prob solution in python

---
+++ Algorithm and DataStructure in Python/FizzBuzz.py
@@ -0,0 +1,37 @@
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+# Example 2:
+
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+# Example 3:
+
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        #empty array list to store the output
+        arr = []
+        
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                arr.append("FizzBuzz")
+            elif i % 3 == 0:
+                arr.append("Fizz")
+            elif i % 5 == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
+        return arr
+        
